10b4a88e165a37d1974cd76605bb3c71cbf460b1
Resi Wicaksono <resiwicaksono98@gmail.com>
2023-01-19 00:49:35 +0700

    auth middleware

--- src/middleware/AuthMiddleware.jsx
+++ src/middleware/AuthMiddleware.jsx
@@ -1,22 +1,20 @@
+/** @format */
+
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { checkUserLogin } from "../features/authSlice";
 
 export const AuthMiddleware = ({ children }) => {
-  const dispatch = useDispatch();
-  const { isLoading } = useSelector((state) => state.auth);
+   const dispatch = useDispatch();
+   const { isLoading } = useSelector((state) => state.auth);
 
-  useEffect(() => {
-    dispatch(checkUserLogin());
-  }, [dispatch]);
+   useEffect(() => {
+      dispatch(checkUserLogin());
+   }, [dispatch]);
 
-  if (isLoading) {
-    return (
-      <div className="flex h-screen items-center justify-center">
-        Loading....
-      </div>
-    );
-  }
+   if (isLoading) {
+      return <div className="flex h-screen items-center justify-center">Loading....</div>;
+   }
 
-  return <div>{children}</div>;
+   return <div>{children}</div>;
 };
